e6461d8ef04a344d532a10167ff1062f6dac1c95
Irish Carpio <IrishCarpio-dev@github.com>
2026-03-02 15:38:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/thehungrydeveloper/StartersActivity.java
@@ -0,0 +1,46 @@
+package com.example.thehungrydeveloper;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StartersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_starters);
+
+        ListView startersList = findViewById(R.id.list_view_starters);
+
+
+        Dish[] dishes = {
+                new Dish("Mushroom and tofu maki", "Toasted seaweed wrapped around sushi rice, filled with chestnut mushroom and smoke tofu", 999),
+                new Dish("Melon and lemon soup", "Fresh melon and lemon combined into creamy soup", 1199),
+                new Dish("Coconut and chocolate mousse", "A creamy mousse made with fresh coconut and milk chocolate", 899),
+                new Dish("Spinach and cabbage wontons", "Thin wonton cases stuffed with fresh spinach and chinese cabbage", 799),
+                new Dish("Broccoli and cucumber soup", "Fresh broccoli and cucumber combined into creamy soup", 899),
+                new Dish("Chilli and aubergine dip", "A dip made from scotch bonnet chilli and fresh aubergine", 999),
+                new Dish("Chickpea and chilli gyoza", "Thin pastry cases stuffed with fresh chickpea and green chilli", 699),
+                new Dish("Sprout and pineapple soup", "Fresh sprout and pineapple combined into creamy soup", 899),
+                new Dish("Egusi and borscht soup", "Egusi and borscht combined into creamy soup", 1299),
+                new Dish("Egg and Ham Sandwich", "A fluffy omelette made with freshly made ham. With wheat buns", 500)
+        };
+
+
+
+        ArrayAdapter<Dish> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dishes);
+
+        startersList.setAdapter(dishesAdapter);
+
+
+    }
+
+}
