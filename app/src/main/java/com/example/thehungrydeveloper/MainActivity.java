5b69f344daa3037a053fc8b68841d5e2d66f4e9f
Irish Carpio <IrishCarpio-dev@github.com>
2026-03-02 20:06:51 +0800

    desserts

--- app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.thehungrydeveloper;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     CardView startersCard;
     CardView mainsCard;
+    CardView dessertsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_main);
+        dessertsCard = findViewById(R.id.card_view_desserts);
 
         startersCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +37,36 @@ public class MainActivity extends AppCompatActivity {
                 Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
 
                 startActivity(startersActivityIntent);
+            }
+        });
+
+        mainsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mainCoursesActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
+
+                startActivity(mainCoursesActivityIntent);
+            }
+        });
+
+        dessertsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dessertActivityIntent = new Intent(MainActivity.this, DessertsActivity.class);
+
+                startActivity(dessertActivityIntent);
+            }
+        });
+
+        TextView emailTextView = findViewById(R.id.text_view_email_address);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchEmailAppIntent = new Intent(Intent.ACTION_SENDTO);
+                launchEmailAppIntent.setData(Uri.parse("mailto:thehungrydev@exampleCarpio.com"));
+                startActivity(launchEmailAppIntent);
+
             }
         });
 
